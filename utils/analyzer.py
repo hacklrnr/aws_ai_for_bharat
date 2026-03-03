30013c79dd2d13752890abbe3f7231dd1bc913de
hacklrnr <vivekadityabhargava@gmail.com>
2026-03-04 00:18:25 +0530

    Update analyzer.py final

--- utils/analyzer.py
+++ utils/analyzer.py
@@ -8,7 +8,9 @@ load_dotenv()
 
 bedrock = boto3.client(
     service_name="bedrock-runtime",
-    region_name=os.getenv("AWS_REGION", "us-east-1")
+    region_name=os.getenv("AWS_REGION", "us-east-1"),
+    aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
+    aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
 )
 
 
